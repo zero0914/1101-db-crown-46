6444ae8abea3b60be0cc82f0eb04a515c2bfb5bb
zero0914 <208417146@gms.tku.edu.tw>
2021-10-13 20:13:48 +0800

    modify database.js to connect to Heroku PostgreSQL

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,17 @@
 const { Pool } = require('pg')
+const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'crown_46',
-  password: '0000',
-  port: 5432,
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'crown46',
+  // password: '0000',
+  // port: 5432,
+  
+  connectionString: isProduction? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown46`
 });
 
-pool.query('SELECT * from category_46', (err, res) => {
+pool.query('SELECT * from category', (err, res) => {
   console.log(JSON.stringify (res.rows))
   pool.end()
 });
