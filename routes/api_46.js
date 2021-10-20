b5f275bd6d2da44867041d70992ca8b2e5eca63f
zero0914 <208417146@gms.tku.edu.tw>
2021-10-20 21:01:41 +0800

    add /api_46/category_462

--- routes/api_46.js
+++ routes/api_46.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const apicrown2Controller_46 = require('../controllers/apicrown2Controller_46')
+const apiCrown2Controller_46 = require('../controllers/apiCrown2Controller_46')
 
 /* GET home page. */
-router.get('/category_46',apicrown2Controller_46.getCategories);
+router.get('/category_46',apiCrown2Controller_46.getCategories);
 
 module.exports = router;
