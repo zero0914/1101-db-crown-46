9168806971388384a21fb55e786dcb7ec8050504
zero0914 <208417146@gms.tku.edu.tw>
2021-10-21 18:32:12 +0800

    Enable API CORS access

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: ['https://heroku-crown46.herokuapp.com/']
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/crown_46', crown_46_Router);
